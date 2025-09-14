6e96e8132a6d6ab88cb9ea61b5353767e6940892
5ohail <sohailansarisa318@gmail.com>
2025-09-14 22:56:24 +0530

    first MVP

---
+++ BACKEND/models/issueModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+ const IssueSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  category: { type: String, required: true },
+  status: { type: String, enum: ["Pending", "Resolved", "Rejected"], default: "Pending" },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+  address: { type: String, required: true },
+  reportedBy: { type: String, required: true }, // userId reference
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+const issueModel = mongoose.model('Issue', IssueSchema);
+
+module.exports = issueModel;
